effc0c3c2c1d927d37d8ff1df116ecef76c8d7e2
Derek <legato_5@yahoo.com>
2020-01-26 19:33:19 -0800

    endpoints currently working for project. moving on to answering questions.

--- data/routers/projects-router.js
+++ data/routers/projects-router.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const Projects = require('../helpers/projectModel.js');
-
+const Actions = require('../helpers/actionModel.js');
 const router = express.Router();
 
 //GET
@@ -31,4 +31,112 @@ router.get('/:id', (req, res) => {
         })
 })
 
+//POST request
+router.post('/', (req, res) => {
+    const newProject = req.body
+    if(newProject.name && newProject.description) {
+        Projects.insert(newProject)
+            .then(project => {
+                res.status(201).json(project)
+            })
+            .catch(err => {
+                res.status(500).json({messsage: "There was an error creating the project"})
+            })
+    }
+    else {
+        res.status(400).json({message: "Please provide the name and description for the project"})
+    }
+})
+
+//Delete request
+router.delete('/:id', (req, res) => {
+    const {id} = req.params;
+
+    Projects.remove(id)
+        .then(deleted => {
+            if(deleted) {
+                res.status(204).end()
+            }
+            else {
+                res.status(404).json({message: "The project with the specified ID does not exist"})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({error: "The project could not be removed."})
+        })
+})
+
+//Put request
+router.put("/:id", (req, res) => {
+    const {id} = req.params;
+    const changes = req.body;
+
+    Projects.update(id, changes)
+        .then(updated => {
+            if(updated) {
+                if(changes.name && changes.description) {
+                    res.status(200).json(updated)
+                }
+                else{
+                    res.status(400).json({errorMessage: "Please provide a name and description."})
+                }
+            }
+            else {
+                res.status(404).json({message: "The project with the specified ID does not exist."})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({error: "The project information could not be modified."})
+        })
+})
+
+
+//get and post actions
+router.get('/:id/actions', validateProjectId, (req, res) => {
+    Projects.getProjectActions(req.project.id)
+      .then(actions => {
+        res.status(200).json(actions)
+      })
+      .catch(err => {
+        res.status(500).json({message: "error retrieving the actions."})
+      })
+  });
+
+router.post('/:id/actions', validateProjectId, (req, res) => {
+    const newAction = req.body
+    newAction.project_id = req.params.id
+    if(newAction.description && newAction.notes) {
+        Actions.insert(newAction)
+            .then(action => {
+                res.status(201).json(action)
+            })
+            .catch(err => {
+                res.status(500).json({messsage: "There was an error creating the action"})
+            })
+    }
+    else {
+        res.status(400).json({message: "Please provide the desription and notes for the action"})
+    }
+  });
+ 
+  
+//validate ProjectID middleware
+function validateProjectId(req, res, next) {
+    const {id} = req.params;
+    Projects.get(id)
+      .then(project => {
+        if(project) {
+          req.project=project
+          next();
+        }
+        else {
+          res.status(404).json({message: "ID does not exist"})
+        }
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(500).json({message: "Exception", err})
+      })
+
+  }
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const projectRouter = require('./data/routers/projects-router.js');
+const actionRouter = require('./data/routers/actions-router.js');
 const helmet = require('helmet')
 
 const server = express();
@@ -7,6 +8,6 @@ const server = express();
 server.use(express.json());
 
 server.use('/api/projects', projectRouter)
-
+server.use('/api/projects/:id/actions', actionRouter)
 
 module.exports = server;
